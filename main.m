04d921dc0e6991a5e6212bbe52fae2b87527e202
mgameiro <miguelgameiro94@hotmail.com>
2016-12-03 04:01:32 +0000

    Aplicação de PCA

--- main.m
+++ main.m
@@ -4,7 +4,8 @@ clear all;
 
 %% Abertura do ficheiro da musica
 %Recebe musica a abrir
-filename = input('Ficheiro: ');
+%filename = input('Ficheiro: ');
+filename = 'Sacrifice.wav';
 %Abre ficheiro da musica
 [musica,fs] = audioread(filename);
 %plot (musica)
@@ -12,3 +13,6 @@ filename = input('Ficheiro: ');
 tempo = length(musica)*(1/fs);
 fprintf('Duração da musica: %d min e %g s\n', floor(tempo/60), (tempo/60-floor(tempo/60))*60);
 %% Geração de espectrograma do sinal da musica
+specmusic = spectrogram(musica,'yaxis');
+%% Aplicação de PCA
+pcamusic = pca(specmusic);
